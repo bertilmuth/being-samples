333d51a19906b7dcff1d75d41622669e42e043ce
bertilmuth <Bertil.Muth@HOOD-Group.com>
2021-02-02 10:12:18 +0100

    Update Greeting.java

--- greetuser/greetuser-impl/src/main/java/org/requirementsascode/being/greetuser/impl/Greeting.java
+++ greetuser/greetuser-impl/src/main/java/org/requirementsascode/being/greetuser/impl/Greeting.java
@@ -2,17 +2,33 @@ package org.requirementsascode.being.greetuser.impl;
 
 import java.time.LocalDateTime;
 
-import org.requirementsascode.being.Properties;
+import lombok.EqualsAndHashCode;
 
-import lombok.Value;
-
-@Value @Properties
+@EqualsAndHashCode
 class Greeting{
-  String id;
-  String text;
-  String timestamp;
+  private final String id;
+  private final String text;
+  private final String timestamp;
+  
+  private Greeting(String id, String text, String timestamp) {
+    this.id = id;
+    this.text = text;
+    this.timestamp = timestamp;
+  }
 
   public static Greeting create(String id, String text) {
     return new Greeting(id, text, LocalDateTime.now().toString());
+  }
+  
+  public String getId() {
+    return id;
+  }
+
+  public String getText() {
+    return text;
+  }
+
+  public String getTimestamp() {
+    return timestamp;
   }
 }
