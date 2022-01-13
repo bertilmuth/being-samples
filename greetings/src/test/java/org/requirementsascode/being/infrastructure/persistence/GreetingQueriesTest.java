90d5e2fd4e3f03e2b4ffb29e2dd653901ad9abad
Bertil Muth <bertilmuth@yahoo.de>
2022-01-13 16:34:51 +0100

    Remove unused and update rest assured dependency

--- greetings/src/test/java/org/requirementsascode/being/infrastructure/persistence/GreetingQueriesTest.java
+++ greetings/src/test/java/org/requirementsascode/being/infrastructure/persistence/GreetingQueriesTest.java
@@ -5,7 +5,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
-import java.util.function.Supplier;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
